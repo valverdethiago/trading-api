2e17131d86d153951ff3f278331a3c9fbb093d81
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2021-03-06 13:02:51 -0300

    Externalizing app configs

--- main.go
+++ main.go
@@ -7,19 +7,18 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/valverdethiago/trading-api/api"
 	db "github.com/valverdethiago/trading-api/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:golang@localhost:6432/trade?sslmode=disable"
-	serverAddress = "0.0.0.0:8081"
+	"github.com/valverdethiago/trading-api/util"
 )
 
 var queries *db.Queries
 
 func main() {
+	config, err := util.LoadConfig(".", "app")
+	if err != nil {
+		log.Fatal("Error loading application config:", err)
+	}
 
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to the database", err)
 	}
@@ -27,7 +26,7 @@ func main() {
 
 	server := api.NewServer(queries)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("Failed to start HTTP server")
